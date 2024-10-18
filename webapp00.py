4ec09bb09167c0241628b9f8bbae301e45a8ebc2
Gabriel Santos <163887701+Gabrielldosantos@users.noreply.github.com>
2024-10-18 20:33:19 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -16,7 +16,7 @@ st.header("Ciencias da Computação")
 st.subheader("Sub Cabeçalho")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Como já deve ter percebido, eu curso CC!")
 
 values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
 st.write("Values:", values)
